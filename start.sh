2ee54d9f61a534524ed82352b211ee34823bfb51
abhinemm <85041628+abhinemm@users.noreply.github.com>
2025-10-01 01:52:50 +0530

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+echo "=== DEBUG: Environment Variables ==="
+echo "RANKMINT_HOST: $RANKMINT_HOST"
+echo "WORDPRESS_HOST: $WORDPRESS_HOST"
+echo "===================================="
+
 envsubst '${RANKMINT_HOST} ${WORDPRESS_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+echo "=== DEBUG: Generated nginx.conf ==="
+cat /etc/nginx/nginx.conf
+echo "===================================="
+
 nginx -g 'daemon off;'
