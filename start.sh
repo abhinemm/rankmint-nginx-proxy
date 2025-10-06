00a1da988ee7e3f88052407ab646621334a14556
abhinemm <85041628+abhinemm@users.noreply.github.com>
2025-10-07 02:54:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "=== DEBUG: Environment Variables ==="
 echo "RANKMINT_HOST: $RANKMINT_HOST"
 echo "WORDPRESS_HOST: $WORDPRESS_HOST"
 echo "==================================="
-envsubst '${RANKMINT_HOST} ${WORDPRESS_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+cp /etc/nginx/nginx.conf.template /etc/nginx/nginx.conf
 echo "=== DEBUG: Generated nginx.conf ==="
 cat /etc/nginx/nginx.conf
 echo "========== NGINX SYNTAX TEST =========="
